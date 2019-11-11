612806cc035f6d614937c4b808cf4325b5e5b119
thisoban mahalingam <thisoban_1995@hotmail.com>
2019-11-11 19:46:55 +0100

    Add project files.

---
+++ AlgorithmShip/ship.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AlgorithmShip
+{
+    public class ship
+    {
+        public ship()
+        {
+            // ship has horizontal list and vertical list
+            // ship need containerlist
+            //split ship
+
+        }
+    }
+}
