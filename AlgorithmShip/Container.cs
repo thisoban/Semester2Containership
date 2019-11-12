a32040be6ed488f412a494a572e4d83330c4fb31
thisoban mahalingam <thisoban_1995@hotmail.com>
2019-11-12 15:55:10 +0100

    added class container and added clas type of container

---
+++ AlgorithmShip/Container.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmShip
+{
+   public class Container
+    {
+        public int Weight { get; set; }
+        public enum Type { get; set; }
+    }
+}
---
+++ AlgorithmShip/Type.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmShip
+{
+    public enum Type
+    {
+        cooled, normal, valuable
+    }
+}
